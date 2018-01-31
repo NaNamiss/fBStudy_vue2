d9090cebd7b971f971526e49c9f1ed77fb032d76
wangnana <wangnana@idwzx.com>
2018-01-31 15:07:06 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
     <v-header></v-header>
     <div class="tab">
       <div class="tab-item">
-        <router-link to='/goods'>商品</router-link>
+        <router-link to='/goods'>商品1</router-link>
       </div>
       <div class="tab-item">
-        <router-link to='/ratings'>评论</router-link>
+        <router-link to='/ratings'>评论2</router-link>
       </div>
       <div class="tab-item">
-        <router-link to='/seller'>商家</router-link>
+        <router-link to='/seller'>商家3</router-link>
       </div>
     </div>
 
